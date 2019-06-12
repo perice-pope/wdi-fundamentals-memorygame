24dcff4173f1fe0929beebb2885f852aa1e5bd08
Perice Pope <pericepope@imac.attlocal.net>
2019-06-11 18:24:06 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
-console.log("Up and running!"); 
+// console.log("Up and running!"); 
+cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+]; 
 
-cards = ["king", "king", "queen", "queen"]; 
 cardsInPlay = [];
 
 function checkForMatch() {
@@ -9,14 +30,21 @@ function checkForMatch() {
 } else {
   console.log("Sorry, try again.");
 }
-}
+};
 
 function flipCard(cardId) {
 checkForMatch(); 
+console.log("user flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 
-console.log("user flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log(cards.cardImage);
+console.log(cards.suit);
 }
 flipCard(0); 
 flipCard(2); 
-flipCard(2); 
+
+
+
+
+
+
